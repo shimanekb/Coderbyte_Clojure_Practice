06a4593aa1729c23215f9498812902da36253995
shimanekb <brandon.shimanek@hotmail.com>
2015-08-06 18:13:24 -0500

    empty project for simple adding challenge

---
+++ simple-adding/test/simple_adding/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-adding.core-test
+  (:require [clojure.test :refer :all]
+            [simple-adding.core :refer :all]))
+
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
