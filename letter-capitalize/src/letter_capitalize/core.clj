79f378ae02bf5c4e42380af5dff2d3d74fb67ea9
shimanekb <brandon.shimanek@hotmail.com>
2015-08-08 13:57:00 -0500

    finished challenge

--- letter-capitalize/src/letter_capitalize/core.clj
+++ letter-capitalize/src/letter_capitalize/core.clj
@@ -1,7 +1,11 @@
 (ns letter-capitalize.core
+  (:use [letter-capitalize.letter-cap :only (capitalize-letters)])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (loop [value (do
+                 (println "Enter a sentence:")
+                 (read-line))]
+    (println (capitalize-letters value))))
